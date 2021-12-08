b0f0ccf39b71c1cf517dec55d74fbe9ca88d06ff
Jinyi Xian <kylerky@live.com>
2021-12-08 15:50:50 +0100

    Revert "Remove the unnecessary comment"
    This reverts commit df7b2f7a3ee022f067830932b492db483d69f027.

--- verified/src/main/scala/runtime/Executor.scala
+++ verified/src/main/scala/runtime/Executor.scala
@@ -42,6 +42,24 @@ object Executor {
     require(s0.taskNum == s1.taskNum && scheduleValid(schedule, s0))
   }.ensuring(scheduleValid(schedule, s1))
 
+  // def scheduleValidPropagates[S, T](
+  //     schedule: Schedule,
+  //     history: History[S, T]
+  // ): Unit = {
+  //   require(history.nonEmpty)
+  //   require(historyTaskNumEqual(history, history.head))
+  //   require(scheduleValid(schedule, history.head))
+
+  //   history match {
+  //     case Cons(s, Nil()) =>
+  //     case Cons(s, rest) => {
+  //       historyTaskNumEqualSubstitution(history, s, rest.head)
+  //       scheduleValidStateContentAgnostic(schedule, s, rest.head)
+  //       scheduleValidPropagates(schedule, rest)
+  //     }
+  //   }
+  // }.ensuring(history.forall(scheduleValid(schedule, _)))
+
   def runOne[S, T](
       f: (S, T) => (S, T),
       state: State[S, T],
@@ -78,6 +96,94 @@ object Executor {
     require(maintainInvariant(predicate, f, state, choice))
   }.ensuring(predicate(state) ==> predicate(runOne(f, state, choice)))
 
+  // def runConsistent[S, T](
+  //     f: (S, T) => (S, T),
+  //     schedule: Schedule,
+  //     history: History[S, T]
+  // ): Boolean = {
+  //   require(history.nonEmpty)
+
+  //   history.length == (schedule.length + 1) &&
+  //   historyTaskNumEqual(history, history.head) &&
+  //   history.forall(scheduleValid(schedule, _)) &&
+  //   runConsistentStepWise(f, schedule, history) &&
+  //   runConsistentChoice(schedule, history)
+  // }
+
+  // def runConsistentStepWise[S, T](
+  //     f: (S, T) => (S, T),
+  //     schedule: Schedule,
+  //     history: History[S, T]
+  // ): Boolean = {
+  //   require(history.nonEmpty)
+  //   history
+  //     .zip(history.tail)
+  //     .zip(schedule)
+  //     .forall { case ((s0, s1), c) => runConsistentStepOneChoice(f, s0, s1, c) }
+  // }
+
+  // def runConsistentStepOne[S, T](
+  //     f: (S, T) => (S, T),
+  //     sOld: State[S, T],
+  //     sNew: State[S, T],
+  //     choice: TaskChoice
+  // ): Boolean = {
+  //   require(choiceValid(choice, sOld))
+  //   sNew == runOne(f, sOld, choice)
+  // }
+
+  // def runConsistentStepOneChoice[S, T](
+  //     f: (S, T) => (S, T),
+  //     sOld: State[S, T],
+  //     sNew: State[S, T],
+  //     choice: TaskChoice
+  // ): Boolean = {
+  //   choiceValid(choice, sOld) && runConsistentStepOne(f, sOld, sNew, choice)
+  // }
+
+  // def runConsistentStepOnePredicate[S, T](
+  //     predicate: State[S, T] => Boolean,
+  //     f: (S, T) => (S, T),
+  //     sOld: State[S, T],
+  //     sNew: State[S, T],
+  //     choice: TaskChoice
+  // ): Boolean = {
+  //   require(choiceValid(choice, sOld))
+  //   require(maintainInvariant(predicate, f, sOld, choice))
+  //   require(runConsistentStepOneChoice(f, sOld, sNew, choice))
+
+  //   runOneMaintainsInvariant(predicate, f, sOld, choice)
+
+  //   predicate(sOld) ==> predicate(sNew)
+  // }.holds
+
+  // def runConsistentChoice[S, T](
+  //     schedule: Schedule,
+  //     history: History[S, T]
+  // ): Boolean = {
+  //   require(history.nonEmpty)
+  //   history
+  //     .zip(history.tail)
+  //     .zip(schedule)
+  //     .forall { case ((s0, s1), c) => choiceValid(c, s0) }
+  // }
+
+  // def historyTaskNumEqual[S, T](
+  //     history: History[S, T],
+  //     s: State[S, T]
+  // ): Boolean = {
+  //   history.forall(_.taskNum == s.taskNum)
+  // }
+
+  // def historyTaskNumEqualSubstitution[S, T](
+  //     @induct history: History[S, T],
+  //     s0: State[S, T],
+  //     s1: State[S, T]
+  // ): Unit = {
+  //   require(historyTaskNumEqual(history, s0))
+  //   require(s0.taskNum == s1.taskNum)
+  // }.ensuring(historyTaskNumEqual(history, s1))
+
   def run[S, T](
       f: (S, T) => (S, T),
       state: State[S, T],
@@ -91,9 +197,17 @@ object Executor {
         val newState = runOne(f, state, choice)
 
         scheduleValidStateContentAgnostic(rest, state, newState)
-        state :: run(f, newState, rest)
+        val following = run(f, newState, rest)
+
+        val res = state :: following
+
+        // historyTaskNumEqualSubstitution(res, newState, state)
+        // scheduleValidPropagates(schedule, res)
+        res
       }
     }
+
+    // }.ensuring(res => runConsistent(f, schedule, res))
   }
 
   def historyMaintainsInvariant[S, T](
@@ -103,6 +217,7 @@ object Executor {
       schedule: Schedule
   ): Boolean = {
     require(history.nonEmpty)
+    // require(runConsistent(f, schedule, history))
 
     history.zip(schedule).forall { case (s, c) =>
       choiceValid(c, s) && maintainInvariant(predicate, f, s, c)
@@ -123,7 +238,8 @@ object Executor {
       case Nil() => List(state)
       case Cons(choice, rest) => {
         val newState = runOne(f, state, choice)
-        state :: runMaintainsInvariant(predicate, f, newState, rest)
+        val following = runMaintainsInvariant(predicate, f, newState, rest)
+        state :: following
       }
     }
 
