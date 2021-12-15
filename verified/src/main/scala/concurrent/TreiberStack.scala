982060c7c561449adf5de7de607297872b38e92a
Jinyi Xian <kylerky@live.com>
2021-12-15 11:45:15 +0100

    Prove the correctness of TreiberStack

--- verified/src/main/scala/concurrent/TreiberStack.scala
+++ verified/src/main/scala/concurrent/TreiberStack.scala
@@ -2,24 +2,34 @@ package concurrent
 
 import stainless.lang._
 import stainless.collection._
+import stainless.annotation._
 
 import runtime.Executor
 import runtime.Executor.{State, Schedule, History, TaskChoice}
 
 object TreiberStack {
-  type Shared = Int
-  type TaskState = Int
-  type StackState = State[Shared, TaskState]
+  sealed abstract class TaskState[T]
+  case class PushRead[T](v: T) extends TaskState[T]
+  case class PushTry[T](v: T, oldHead: Option[Node[T]]) extends TaskState[T]
+  case class PopRead[T]() extends TaskState[T]
+  case class PopTry[T](oldHead: Option[Node[T]]) extends TaskState[T]
 
-  def nop(x: Int, y: Int) = { (x, y) }.ensuring(res =>
-    res._1 == x && res._2 == y
+  case class Node[T](value: T, next: Option[Node[T]])
+
+  case class Shared[T](
+      head: Option[Node[T]],
+      @ghost
+      reference: List[T]
   )
 
-  // val initState: State[Int, Int] = State(0, List(0, 0))
+  type StackState[T] = State[Shared[T], TaskState[T]]
 
-  def nopTaskStateEqual(
-      oldState: StackState,
-      newState: StackState,
+  def nop[T](x: Shared[T], y: TaskState[T]) = { (x, y) }
+    .ensuring(res => res._1 == x && res._2 == y)
+
+  def nopTaskStateEqual[T](
+      oldState: StackState[T],
+      newState: StackState[T],
       choice: TaskChoice
   ): Unit = {
     require(choice >= 0)
@@ -44,45 +54,160 @@ object TreiberStack {
     }
   }.ensuring(oldState.tasks == newState.tasks)
 
-  def run(schedule: Schedule, s: StackState): List[StackState] = {
+  def runNop[T](schedule: Schedule, s: StackState[T]): List[StackState[T]] = {
     require(Executor.scheduleValid(schedule, s))
 
-    val pred = (s0: StackState) => s0 == s
+    def pred(s0: StackState[T]) = { s0 == s }
 
     val res = Executor
-      .runMaintainsInvariant(pred, nop, s, schedule)
+      .runMaintainsInvariant(pred, nop[T], s, schedule)
 
     def nopDoesNothing(
-        history: History[Shared, TaskState],
+        history: History[Shared[T], TaskState[T]],
         schedule: Schedule
     ): Unit = {
+      decreases(history.length)
+
       require(history.nonEmpty)
-      require(Executor.runConsistent(nop, schedule, history))
+      require(
+        Executor.runConsistent(nop[T], schedule, history)
+      )
+      assert(history.length == schedule.length + 1)
+
+      schedule match {
+        case Nil() =>
+        case Cons(choice, rest) => {
+
+          assert(Executor.choiceValid(choice, history.head))
+          val state = history.head
+          val newState = Executor.runOne(nop[T], state, choice)
+          assert(newState.shared == state.shared)
+
+          nopTaskStateEqual(state, newState, choice)
+          assert(newState == state)
+
+          assert(pred(state) ==> pred(Executor.runOne(nop[T], state, choice)))
+
+          Executor.runConsistentSubHistory(nop[T], schedule, history)
+          nopDoesNothing(history.tail, schedule.tail)
+        }
+      }
+    }.ensuring(
+      Executor.historyMaintainsInvariant(pred, nop[T], history, schedule)
+    )
+
+    nopDoesNothing(res, schedule)
+    Executor.predicateForAllPremiseTrue(pred, nop[T], s, res, schedule)
+    assert(Executor.predicateForAll(pred, res))
+
+    res
+  }
+
+  def stackStateValid[T](s: StackState[T]): Boolean = {
+    stackSharedStateValid(s.shared)
+  }
+
+  def stackSharedStateValid[T](shared: Shared[T]): Boolean = {
+    shared match {
+      case Shared(None(), Nil()) => true
+      case Shared(Some(v), Cons(head, rest)) =>
+        (v.value == head) && stackSharedStateValid(Shared(v.next, rest))
+      case Shared(_, _) => false
+    }
+  }
+
+  @ghost
+  def referenceWorkAround[T](reference: List[T], v: T) =
+    if (reference.isEmpty) List(v) else reference
+
+  def stackOp[T](x: Shared[T], y: TaskState[T]): (Shared[T], TaskState[T]) = {
+    y match {
+      case PushRead(v) => (x, PushTry(v, x.head))
+      case PushTry(v, oldHead) => {
+        if (x.head == oldHead) {
+          val node = Node(v, oldHead)
+          (Shared(Some(node), Cons(v, x.reference)), PushRead(v))
+        } else {
+          (x, PushRead(v))
+        }
+      }
+
+      case PopRead() => (x, PopTry(x.head))
+      case PopTry(oldHead) => {
+        oldHead match {
+          case None() => (x, PopRead[T]())
+          case Some(v) => {
+            val newHead = v.next
+            if (x.head == oldHead) {
+              // work around in case `reference` is empty
+              @ghost
+              val reference = referenceWorkAround(x.reference, v.value)
+              (Shared(newHead, reference.tail), PopRead[T]())
+            } else {
+              (x, PopRead[T]())
+            }
+          }
+        }
+      }
+    }
+  }.ensuring(res => stackSharedStateValid(x) ==> stackSharedStateValid(res._1))
+
+  def run[T](schedule: Schedule, s: StackState[T]): List[StackState[T]] = {
+    require(Executor.scheduleValid(schedule, s))
+    require(stackStateValid(s))
+
+    val res = Executor
+      .runMaintainsInvariant(stackStateValid[T], stackOp[T], s, schedule)
+
+    def opMaintainsInvariant(
+        history: History[Shared[T], TaskState[T]],
+        schedule: Schedule
+    ): Unit = {
+      decreases(history.length)
+
+      require(history.nonEmpty)
+      require(
+        Executor.runConsistent(stackOp[T], schedule, history)
+      )
+      assert(history.length == schedule.length + 1)
 
       schedule match {
         case Nil() =>
         case Cons(choice, rest) => {
           assert(Executor.choiceValid(choice, history.head))
           val state = history.head
-          val newState = Executor.runOne(nop, state, choice)
-          assert(newState.shared == state.shared)
+          val newState = Executor.runOne(stackOp[T], state, choice)
 
-          nopTaskStateEqual(state, newState, choice)
-          assert(newState == state)
+          assert(stackStateValid(state) ==> stackStateValid(newState))
 
-          assert(pred(state) ==> pred(Executor.runOne(nop, state, choice)))
+          assert(
+            stackStateValid(state) ==> stackStateValid(
+              Executor.runOne(stackOp[T], state, choice)
+            )
+          )
 
-          Executor.runConsistentSubHistory(nop, schedule, history)
-          nopDoesNothing(history.tail, schedule.tail)
+          Executor.runConsistentSubHistory(stackOp[T], schedule, history)
+          opMaintainsInvariant(history.tail, schedule.tail)
         }
       }
     }.ensuring(
-      Executor.historyMaintainsInvariant(pred, nop, history, schedule)
+      Executor.historyMaintainsInvariant(
+        stackStateValid[T],
+        stackOp[T],
+        history,
+        schedule
+      )
     )
 
-    nopDoesNothing(res, schedule)
-    Executor.predicateForAllPremiseTrue(pred, nop, s, res, schedule)
-    assert(Executor.predicateForAll(pred, res))
+    opMaintainsInvariant(res, schedule)
+    Executor.predicateForAllPremiseTrue(
+      stackStateValid[T],
+      stackOp[T],
+      s,
+      res,
+      schedule
+    )
+    assert(Executor.predicateForAll(stackStateValid[T], res))
 
     res
   }
